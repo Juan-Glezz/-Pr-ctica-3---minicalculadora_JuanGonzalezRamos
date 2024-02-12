75e4cd03dcae4b5fe8370e6e44442aa08e1ea7f7
Juan-Glezz <jgonram1210@iesmartinezm.es>
2024-02-12 14:43:45 +0100

    Practica 3 Mini Calculadora

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+  num1: number=0;
+  num2: number=0;
+  result: number=0;
+
+  sumar():void {
+    this.result = this.num1 + this.num2;
+  }
+
+  restar():void  {
+    this.result = this.num1 - this.num2;
+  }
+
+}
